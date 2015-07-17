0a36dbbabdd1bfda41e97b2562b81fdeef80172d
cherno <matt@chernosky.net>
2015-07-16 23:43:18 -0600

    Updated main to use the LED module to control the LEDs.

--- example-project/src/main.c
+++ example-project/src/main.c
@@ -1,11 +1,6 @@
-
 #include <stdint.h>
-#include <stdbool.h>
-#include "inc/tm4c123gh6pm.h"
-#include "inc/hw_memmap.h"
-#include "driverlib/pin_map.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/gpio.h"
+
+#include "led.h"
 
 //*****************************************************************************
 //
@@ -17,43 +12,20 @@ main(void)
 {
     volatile uint32_t ui32Loop;
 
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    LedInit();
 
-    //
-    // Enable the GPIO pin for the LED (PF3).  Set the direction as output, and
-    // enable the GPIO pin for digital function.
-    //
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
-
-
-    //
-    // Loop forever.
-    //
     while(1)
     {
-        //
-        // Turn on the LED.
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
+        LedSetBlue();
 
-        //
         // Delay for a bit.
-        //
         for(ui32Loop = 0; ui32Loop < 400000; ui32Loop++)
         {
         }
 
-        //
-        // Turn off the LED.
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
+        LedSetOff();
 
-        //
         // Delay for a bit.
-        //
         for(ui32Loop = 0; ui32Loop < 400000; ui32Loop++)
         {
         }
